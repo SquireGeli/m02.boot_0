4c60628a7a21331f87f9062630323711a3f780cc
squire geli <fegtordera@gmail.com>
2021-01-27 20:21:19 +0100

    Objetos

---
+++ objetos.py
@@ -0,0 +1,19 @@
+import turtle
+
+tortuguita = turtle.Turtle()
+otraTortuguita = turtle.Turtle()
+masTurtle = turtle.Turtle()
+
+tortuguita.shape('turtle')
+tortuguita.fd(50)
+
+otraTortuguita.shape('turtle')
+otraTortuguita.color('green')
+otraTortuguita.left(90)
+otraTortuguita.fd(50)
+
+masTurtle.shape('turtle')
+masTurtle.color('red')
+masTurtle.left(180)
+masTurtle.fd(150)
+

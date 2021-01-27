714fb05bddfcb213a1d308b64e9f47adb59aef86
squire geli <fegtordera@gmail.com>
2021-01-28 00:00:25 +0100

    Recursividad

---
+++ retrocontador.py
@@ -0,0 +1,11 @@
+'''La recursividad es una funcion que para repetirse se invoca a si misma'''
+def retrocontador (e):
+    print ("{},". format(e), end="")
+    if e == 0:
+        return
+    retrocontador (e -1)
+    
+retrocontador(10)
+'''Si no se le indica que termine en el 0, el programa entraria en un bucle infinito hasta bloquearnos.
+El segundo punto importante de la recursividad es que siempre haya un punto de parada, donde termine de
+autollamarse a si misma'''

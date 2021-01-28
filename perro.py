2539862a9a9c39b9ec747faf0fe5ed2e998148fd
squire geli <fegtordera@gmail.com>
2021-01-29 00:38:21 +0100

    Perro act

--- perro.py
+++ perro.py
@@ -3,4 +3,21 @@ class Perro():
         self.nombre = nombre
         self.edad = edad
         self.peso = peso
+    def ladrar(self):
+        if self.peso >= 8:
+            print ("GUAU,GUAU")
+        else:
+            print ("Guau,guau")
+    def __str__(self):
+         return "Soy el perro: {},tengo: {} años, y peso: {}kg"\
+              .format(self.nombre,self.edad,self.peso)
         
+def ladrar():
+    print("No tengo perro")
+    
+Maig = Perro("Maig",8,40)
+Max = Perro("Max",5,4)
+print(Maig, "y ladro asi:")
+Maig.ladrar()
+print(Max, "y ladro asi:")
+Max.ladrar()

b2ddcd3f540127ba0c862b7968b37f9b3d02e612
squire geli <fegtordera@gmail.com>
2021-01-29 01:39:45 +0100

    Perro Terminado

--- perro.py
+++ perro.py
@@ -11,13 +11,41 @@ class Perro():
     def __str__(self):
          return "Soy el perro: {},tengo: {} años, y peso: {}kg"\
               .format(self.nombre,self.edad,self.peso)
+class Perroasistencia(Perro):
+    def __init__(self, nombre,edad,peso, amo):
+        Perro.__init__(self,nombre,edad,peso)
+        self.amo = amo
+        self.__trabajando = False
+    def __str__(self):
+         return "Soy el perro de asistencia: {},tengo: {} años, y peso: {}kg y mi dueño es: {}"\
+              .format(self.nombre,self.edad,self.peso,self.amo)
+    def pasear(self):
+        print("Y yo {} que soy perro de asistencia, ayudo a mi dueño {} a pasear".format(self.nombre,self.amo))
+
+    def ladrar(self):
+        if self.__trabajando:
+            print("No puedo ladrar porque estoy trabajando")
+        else:
+            Perro.ladrar(self)
+            
+    def trabajando(self, valor=None):
+        if valor == None:
+            return self.__trabajando
+        else:
+            self.__trabajando= valor
         
+
 def ladrar():
     print("No tengo perro")
     
 Maig = Perro("Maig",8,40)
 Max = Perro("Max",5,4)
+Bjorn = Perroasistencia ("Bjorn",5,30,"Kat")
 print(Maig, "y ladro asi:")
 Maig.ladrar()
 print(Max, "y ladro asi:")
 Max.ladrar()
+print(Bjorn, "y ladro asi:")
+Bjorn.trabajando = True
+Bjorn.ladrar()
+Bjorn.pasear()
